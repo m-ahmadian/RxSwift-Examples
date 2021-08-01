7b260cafbf826a5d93bb556c9ea6ce1ae9ddc602
Mehdad <mehrdad.ahmadian@gmail.com>
2021-07-31 21:09:04 -0400

    Add Chapter 1 - Hello, RxSwift

---
+++ Chapter 1 - Hello, RxSwift!/Asynchronous code/Sample RxSwift ProjectTests/Sample_RxSwift_ProjectTests.swift
@@ -0,0 +1,33 @@
+//
+//  Sample_RxSwift_ProjectTests.swift
+//  Sample RxSwift ProjectTests
+//
+//  Created by Mehrdad Ahmanian on 2021-07-23.
+//
+
+import XCTest
+@testable import Sample_RxSwift_Project
+
+class Sample_RxSwift_ProjectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ Chapter 1 - Hello, RxSwift!/Synchronous code.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var array = [1, 2, 3]
+for number in array {
+    print(number)
+    array = [4, 5, 6]
+}
+
+print(array)
