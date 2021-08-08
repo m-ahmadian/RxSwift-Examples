4ebf73553f6ec84c59d0a4682f65587fba548324
Mehdad <mehrdad.ahmadian@gmail.com>
2021-08-08 18:20:40 -0400

    Wrapping an existing API

--- Chapter 4 - Observables & Subjects in Practice/Starter/combinestagram/PhotoWriter.swift
+++ Chapter 4 - Observables & Subjects in Practice/Starter/combinestagram/PhotoWriter.swift
@@ -27,13 +27,33 @@
 /// THE SOFTWARE.
 
 import Foundation
-import UIKit
 import Photos
+import RxSwift
+import UIKit
 
 class PhotoWriter {
   enum Errors: Error {
     case couldNotSavePhoto
   }
 
+  static func save(_ image: UIImage) -> Observable<String> {
+    return Observable.create { observer in
+      var savedAssetId: String?
+      PHPhotoLibrary.shared().performChanges ({
+        let request = PHAssetChangeRequest.creationRequestForAsset(from: image)
+        savedAssetId = request.placeholderForCreatedAsset?.localIdentifier
+      }, completionHandler: { success, error in
+        DispatchQueue.main.async {
+          if success, let id = savedAssetId {
+            observer.onNext(id)
+            observer.onCompleted()
+          } else {
+            observer.onError(error ?? Errors.couldNotSavePhoto)
+          }
+        }
+      })
+      return Disposables.create()
+    }
+  }
 
 }
