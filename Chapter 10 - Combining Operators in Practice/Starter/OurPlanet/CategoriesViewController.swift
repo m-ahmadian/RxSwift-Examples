1dad07d5deef5f1cb8c8fa5a9a953f2b3c174b51
Mehrdad Ahmadian <mehrdad.ahmadian@gmail.com>
2021-09-07 23:42:02 -0400

    Updating Categories with Events

--- Chapter 10 - Combining Operators in Practice/Starter/OurPlanet/CategoriesViewController.swift
+++ Chapter 10 - Combining Operators in Practice/Starter/OurPlanet/CategoriesViewController.swift
@@ -53,7 +53,21 @@ class CategoriesViewController: UIViewController, UITableViewDataSource, UITable
 
   func startDownload() {
     let eoCategories = EONET.categories
+    let downloadedEvents = EONET.events(forLast: 360)
+
+    let updatedCategories = Observable
+      .combineLatest(eoCategories, downloadedEvents) { (categories, events) -> [EOCategory] in
+        return categories.map { category in
+          var cat = category
+          cat.events = events.filter {
+            $0.categories.contains(where: { $0.id == category.id })
+          }
+          return cat
+        }
+      }
+
     eoCategories
+      .concat(updatedCategories)
       .bind(to: categories)
       .disposed(by: disposeBag)
   }
