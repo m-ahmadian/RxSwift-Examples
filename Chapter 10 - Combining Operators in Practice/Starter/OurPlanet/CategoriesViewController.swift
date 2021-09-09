708fc0420271243a587e1525387f566988f84cc5
Mehrdad Ahmadian <mehrdad.ahmadian@gmail.com>
2021-09-09 16:49:15 -0400

    Incrementally updating the UI

--- Chapter 10 - Combining Operators in Practice/Starter/OurPlanet/CategoriesViewController.swift
+++ Chapter 10 - Combining Operators in Practice/Starter/OurPlanet/CategoriesViewController.swift
@@ -53,18 +53,27 @@ class CategoriesViewController: UIViewController, UITableViewDataSource, UITable
 
   func startDownload() {
     let eoCategories = EONET.categories
-    let downloadedEvents = EONET.events(forLast: 360)
+    let downloadedEvents = eoCategories
+      .flatMap { categories in
+        return Observable.from(categories.map { category in
+          EONET.events(forLast: 360, category: category)
+        })
+      }
+      .merge(maxConcurrent: 2)
 
-    let updatedCategories = Observable
-      .combineLatest(eoCategories, downloadedEvents) { (categories, events) -> [EOCategory] in
-        return categories.map { category in
-          var cat = category
-          cat.events = events.filter {
-            $0.categories.contains(where: { $0.id == category.id })
+    let updatedCategories = eoCategories.flatMap { categories in
+      downloadedEvents.scan(categories) { updated, events in
+        return updated.map { category in
+          let eventsForCategory = EONET.filteredEvents(events: events, forCategory: category)
+          if !eventsForCategory.isEmpty {
+            var cat = category
+            cat.events = cat.events + eventsForCategory
+            return cat
           }
-          return cat
+          return category
         }
       }
+    }
 
     eoCategories
       .concat(updatedCategories)
