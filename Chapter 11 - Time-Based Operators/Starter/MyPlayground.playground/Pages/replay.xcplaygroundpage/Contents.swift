dbf24612675928965987e055d2412b50a77dd7db
Mehrdad Ahmadian <mehrdad.ahmadian@gmail.com>
2021-09-15 20:10:38 -0400

    Timer operators - Intervals

--- Chapter 11 - Time-Based Operators/Starter/MyPlayground.playground/Pages/replay.xcplaygroundpage/Contents.swift
+++ Chapter 11 - Time-Based Operators/Starter/MyPlayground.playground/Pages/replay.xcplaygroundpage/Contents.swift
@@ -31,19 +31,22 @@ let maxElements = 5
 let replayedElements = 1
 let replayDelay: TimeInterval = 3
 
-let sourceObservable = Observable<Int>.create { observer in
-    var value = 1
-    let timer = DispatchSource.timer(interval: 1.0 / Double(elementsPerSecond), queue: .main) {
-        if value <= maxElements {
-            observer.onNext(value)
-            value += 1
-        }
-    }
-    return Disposables.create {
-        timer.suspend()
-    }
-}
-.replayAll()
+//let sourceObservable = Observable<Int>.create { observer in
+//    var value = 1
+//    let timer = DispatchSource.timer(interval: 1.0 / Double(elementsPerSecond), queue: .main) {
+//        if value <= maxElements {
+//            observer.onNext(value)
+//            value += 1
+//        }
+//    }
+//    return Disposables.create {
+//        timer.suspend()
+//    }
+//}
+//.replayAll()
+
+let sourceObservable = Observable<Int>.interval(RxTimeInterval.seconds(Int(1.0 / Double(elementsPerSecond))), scheduler: MainScheduler.instance)
+    .replay(replayedElements)
 
 let sourceTimeline = TimelineView<Int>.make()
 let replayedTimeline = TimelineView<Int>.make()
